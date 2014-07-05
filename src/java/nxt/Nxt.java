ae67938290edd919ec15fd95570beddd7c297958
FIMKrypto <fimkrypto@gmail.com>
2014-07-05 17:58:43 +0200

    Bump version number

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -19,7 +19,7 @@ public final class Nxt {
     public static final String NXT_VERSION = "1.1.5";
     
     /* XXX - This tracks the FIM version */
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.2";
 
     private static final Properties defaultProperties = new Properties();
     static {
