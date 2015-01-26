df3d7ba08f9bbf98dd8710ca237a272883ff3cc0
FIMKrypto <fimkrypto@gmail.com>
2015-01-26 16:57:28 +0100

    Make RewardsImpl re-usable

--- src/java/nxt/RewardsImpl.java
+++ src/java/nxt/RewardsImpl.java
@@ -4,21 +4,24 @@ import nxt.util.Convert;
 
 public class RewardsImpl {
   
-  static public long augmentFee(Block block , long totalFeeNQT) {    
-    long rewardNQT = calculatePOSRewardNQT(block);
-    long totalRewardNQT = Convert.safeAdd(rewardNQT, totalFeeNQT);
-    return totalRewardNQT;
-  }
-  
-  static public long calculatePOSRewardNQT(Block block) {
-    int height = block.getHeight();
-    if (height >= Constants.FORGER_FEE_BLOCK) {
-      for (int i = 0; i < Constants.FORGER_FEE_AMOUNT_NQT_STAGES.length; i++) {
-        if (height < (Constants.FORGER_FEE_STAGE_CHANGE_AT_BLOCK * (i + 1))) {
-          return Constants.FORGER_FEE_AMOUNT_NQT_STAGES[i];
-        }
-      }
+    static public long augmentFee(Block block , long totalFeeNQT) {    
+        long rewardNQT = calculatePOSRewardNQT(block);
+        long totalRewardNQT = Convert.safeAdd(rewardNQT, totalFeeNQT);
+        return totalRewardNQT;
     }
-    return 0;
-  }
+    
+    static public long calculatePOSRewardNQT(Block block) {
+        return calculatePOSRewardNQT(block.getHeight());
+    }
+    
+    static public long calculatePOSRewardNQT(int height) {
+        if (height >= Constants.FORGER_FEE_BLOCK) {
+            for (int i = 0; i < Constants.FORGER_FEE_AMOUNT_NQT_STAGES.length; i++) {
+                if (height < (Constants.FORGER_FEE_STAGE_CHANGE_AT_BLOCK * (i + 1))) {
+                    return Constants.FORGER_FEE_AMOUNT_NQT_STAGES[i];
+                }
+            }
+        }
+        return 0;
+    }
 }
