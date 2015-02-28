c5e4ac1e2c6a630e9f529d4a45a9a78ec6c4e363
FIMKrypto <fimkrypto@gmail.com>
2015-02-28 12:45:10 +0100

    Make UnconfirmedTransaction class public

--- src/java/nxt/UnconfirmedTransaction.java
+++ src/java/nxt/UnconfirmedTransaction.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-class UnconfirmedTransaction implements Transaction {
+public class UnconfirmedTransaction implements Transaction {
 
     private final TransactionImpl transaction;
     private final long arrivalTimestamp;
