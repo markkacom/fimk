cfcca5b01bd8876296186129a3b57951ea3323cd
FIMKrypto <fimkrypto@gmail.com>
2015-03-04 15:47:39 +0100

    Run test suite straight from Eclipse

---
+++ src/hack/nxt/RunTests.java
@@ -0,0 +1,16 @@
+package nxt;
+
+import nxt.http.AssetSuite;
+import org.junit.runner.Computer;
+import org.junit.runner.JUnitCore;
+
+public class RunTests {
+  
+    public static void main(String[] args) {
+        NxtProperties.setup();
+        JUnitCore runner = new JUnitCore();
+        runner.addListener(new org.junit.internal.TextListener(System.out));
+        runner.run(new Computer(), AssetSuite.class);
+    }
+}
+
