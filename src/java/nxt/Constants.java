8c8e8d70a370017903fd4ca4d118b56a9b6e5689
FIMKrypto <fimkrypto@gmail.com>
2015-03-04 12:53:16 +0100

    Rename constant, testnet LAST_KNOWN_BLOCK set to 15000

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -108,12 +108,12 @@ public final class Constants {
     public static final int DIGITAL_GOODS_STORE_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : THIRD_BIRTH_BLOCK;
     public static final int PUBLIC_KEY_ANNOUNCEMENT_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : THIRD_BIRTH_BLOCK;
     public static final int MONETARY_SYSTEM_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : Integer.MAX_VALUE;
-    public static final int LAST_KNOWN_BLOCK = isTestnet ? 0 : 384000;
+    public static final int LAST_KNOWN_BLOCK = isTestnet ? 15000 : 384000;
     public static final int PRIVATE_ASSETS_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : Integer.MAX_VALUE;
     
     /* range 0.000001% to 2000% / 1 - 2000000000 */
-    public static final long MIN_PRIVATE_ASSET_FEE_PERCENTAGE = 0L; 
-    public static final long MAX_PRIVATE_ASSET_FEE_PERCENTAGE = 2000000000L;
+    public static final int MIN_PRIVATE_ASSET_FEE_PERCENTAGE = 0; 
+    public static final int MAX_PRIVATE_ASSET_FEE_PERCENTAGE = 2000000000;
     
     public static final int[] MIN_VERSION = new int[] {0, 3, 3};                
     
