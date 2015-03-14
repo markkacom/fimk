6455157a6e37d0141f9680341a0456b43a6d7168
FIMKrypto <fimkrypto@gmail.com>
2015-03-14 19:41:06 +0100

    Ground work for public key tests

---
+++ test/java/nxt/http/TestPublicKey.java
@@ -0,0 +1,15 @@
+package nxt.http;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import nxt.BlockchainTest;
+
+public class TestPublicKey extends BlockchainTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(true, true);
+    }
+  
+}
