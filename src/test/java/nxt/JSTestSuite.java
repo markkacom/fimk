63a8f88d78c46e23e50c7c95d11a1e88037984c8
FIMKrypto <fimkrypto@gmail.com>
2015-06-06 16:10:31 +0200

    Javascript tests: runners

---
+++ src/test/java/nxt/JSTestSuite.java
@@ -0,0 +1,18 @@
+package nxt;
+
+import org.junit.runner.RunWith;
+
+import uk.co.benjiweber.junitjs.JSRunner;
+import uk.co.benjiweber.junitjs.Tests;
+
+@Tests({
+    "src/test/resources/TestSystemTest.js",
+    "src/test/resources/PrivateAssetTest.js",
+    "src/test/resources/OrderFeeTest.js",
+    "src/test/resources/TradeFeeTest.js",
+    "src/test/resources/RemoveAllowedTest.js",
+    "src/test/resources/InstantExhange.js"
+})
+@RunWith(JSRunner.class)
+public class JSTestSuite {
+}
