cd46c4d6ef1fc417272a35ea6d5bad7c85b59b7f
FIMKrypto <fimkrypto@gmail.com>
2015-06-07 13:01:41 +0200

    Version bump

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -23,7 +23,7 @@ public final class Nxt {
     public static final String APPLICATION = "FIMK";
     
     /* XXX - This tracks the FIM version */
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.5.0";
 
     private static volatile Time time = new Time.EpochTime();
 
