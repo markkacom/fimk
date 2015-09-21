21348862fc7b6a4f12433b5a9f5403c2396336dd
FIMKrypto <fimkrypto@gmail.com>
2015-09-21 14:34:52 +0200

    First go at making an installer for FIMK

--- release-package.sh
+++ release-package.sh
@@ -5,61 +5,51 @@ then
 	echo VERSION not defined
 	exit 1
 fi
-PACKAGE=nxt-client-${VERSION}
+PACKAGE=fimk-client-${VERSION}
 echo PACKAGE="${PACKAGE}"
-CHANGELOG=nxt-client-${VERSION}.changelog.txt
+CHANGELOG=fimk-client-${VERSION}.changelog.txt
 OBFUSCATE=$2
 
-FILES="changelogs conf html lib resource contrib"
-FILES="${FILES} nxt.exe nxtservice.exe"
-FILES="${FILES} 3RD-PARTY-LICENSES.txt AUTHORS.txt COPYING.txt DEVELOPER-AGREEMENT.txt LICENSE.txt"
-FILES="${FILES} DEVELOPERS-GUIDE.md OPERATORS-GUIDE.md README.md README.txt USERS-GUIDE.md"
+FILES="changelogs changelogs-fim conf html lib resource contrib"
+FILES="${FILES} fim.exe fimservice.exe"
+FILES="${FILES} 3RD-PARTY-LICENSES.txt COPYING.txt LICENSE.txt"
+FILES="${FILES} DEVELOPERS-GUIDE.md OPERATORS-GUIDE.md README.txt USERS-GUIDE.md"
 FILES="${FILES} mint.bat mint.sh run.bat run.sh run-tor.sh run-desktop.sh"
-FILES="${FILES} NXT_Wallet.url"
+FILES="${FILES} FIM_Wallet.url"
 
 unix2dos *.bat
 echo compile
 ./compile.sh
 rm -rf html/doc/*
-rm -rf nxt
+rm -rf fimk
 rm -rf ${PACKAGE}.jar
 rm -rf ${PACKAGE}.exe
 rm -rf ${PACKAGE}.zip
-mkdir -p nxt/
-mkdir -p nxt/logs
+mkdir -p fimk/
+mkdir -p fimk/logs
 
-if [ "${OBFUSCATE}" == "obfuscate" ]; 
-then
-echo obfuscate
-/opt/proguard/bin/proguard.sh @nxt.pro
-mv ../nxt.map ../nxt.map.${VERSION}
-else
-FILES="${FILES} classes src"
+FILES="${FILES} src"
 FILES="${FILES} compile.sh javadoc.sh jar.sh package.sh"
 FILES="${FILES} win-compile.sh win-javadoc.sh win-package.sh"
-echo javadoc
-./javadoc.sh
-fi
+#echo javadoc
+#./javadoc.sh
+
 echo copy resources
-cp installer/lib/JavaExe.exe nxt.exe
-cp installer/lib/JavaExe.exe nxtservice.exe
-cp -a ${FILES} nxt
-echo gzip
-for f in `find nxt/html -name *.html -o -name *.js -o -name *.css -o -name *.json  -o -name *.ttf -o -name *.svg -o -name *.otf`
-do
-	gzip -9c "$f" > "$f".gz
-done
-cd nxt
+cp installer/lib/JavaExe.exe fim.exe
+cp installer/lib/JavaExe.exe fimservice.exe
+cp -a ${FILES} fimk
+
 echo generate jar files
-../jar.sh
+./jar.sh
 echo package installer Jar
+cd fimk
 ../installer/build-installer.sh ../${PACKAGE}
 #echo create installer exe
 #../installer/build-exe.bat ${PACKAGE}
 echo create installer zip
 cd -
-zip -q -X -r ${PACKAGE}.zip nxt -x \*/.idea/\* \*/.gitignore \*/.git/\* \*/\*.log \*.iml nxt/conf/nxt.properties nxt/conf/logging.properties
-rm -rf nxt
+zip -q -X -r ${PACKAGE}.zip fimk -x \*/.idea/\* \*/.gitignore \*/.git/\* \*/\*.log \*.iml fimk/conf/nxt.properties fimk/conf/logging.properties
+rm -rf fimk
 
 echo signing zip package
 ../jarsigner.sh ${PACKAGE}.zip
@@ -69,26 +59,27 @@ echo signing jar package
 
 echo creating change log ${CHANGELOG}
 echo -e "Release $1\n" > ${CHANGELOG}
-echo -e "https://bitbucket.org/JeanLucPicard/nxt/downloads/${PACKAGE}.zip\n" >> ${CHANGELOG}
+echo -e "https://github.com/fimkrypto/fimk/releases/download/v${PACKAGE}/fim-${PACKAGE}.zip\n" >> ${CHANGELOG}
 echo -e "sha256:\n" >> ${CHANGELOG}
 sha256sum ${PACKAGE}.zip >> ${CHANGELOG}
 
-echo -e "\nhttps://bitbucket.org/JeanLucPicard/nxt/downloads/${PACKAGE}.jar\n" >> ${CHANGELOG}
+echo -e "\nhttps://github.com/fimkrypto/fimk/releases/download/v${PACKAGE}/${PACKAGE}.jar\n" >> ${CHANGELOG}
 echo -e "sha256:\n" >> ${CHANGELOG}
 sha256sum ${PACKAGE}.jar >> ${CHANGELOG}
 
-#echo -e "https://bitbucket.org/JeanLucPicard/nxt/downloads/${PACKAGE}.exe\n" >> ${CHANGELOG}
-#echo -e "sha256:\n" >> ${CHANGELOG}
-#sha256sum ${PACKAGE}.exe >> ${CHANGELOG}
+echo -e "https://github.com/fimkrypto/fimk/releases/download/v${PACKAGE}/${PACKAGE}.exe\n" >> ${CHANGELOG}
+echo -e "sha256:\n" >> ${CHANGELOG}
+sha256sum ${PACKAGE}.exe >> ${CHANGELOG}
 
-if [ "${OBFUSCATE}" == "obfuscate" ];
-then
-echo -e "\n\nThis is a development release for testing only. Source code is not provided." >> ${CHANGELOG}
-fi
 echo -e "\n\nChange log:\n" >> ${CHANGELOG}
 
-cat changelogs/${CHANGELOG} >> ${CHANGELOG}
+cat changelogs-fim/${CHANGELOG} >> ${CHANGELOG}
 echo >> ${CHANGELOG}
+
+#######################
+# Skip signing for now
+exit 0
+#######################
 
 gpg --detach-sign --armour --sign-with jlp666@yandex.ru ${PACKAGE}.zip
 gpg --detach-sign --armour --sign-with jlp666@yandex.ru ${PACKAGE}.jar
