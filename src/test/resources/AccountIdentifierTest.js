374bb861691462dc5f5e5e68ef7bc73ea9e55af7
FIMKrypto <fimkrypto@gmail.com>
2015-12-29 15:10:37 +0100

    Account Identifier js tests

--- src/test/resources/AccountIdentifierTest.js
+++ src/test/resources/AccountIdentifierTest.js
@@ -18,18 +18,48 @@ tests({
     jsAssert.assertEquals(['bb@fimk.fi','aa@fimk.fi'], this.account1.getAccountIdentifiers());    
   },
   "Other account can set ID when providing signature": function () {
-    return;
+
     // other accounts can assign an id to an account when they can provide a 
     // signed message of the desired id, signed by the account the id is assigned to 
-    
+
     // the account having it's id assigned signs the id with his secret key
     var signature = Nxt.util.sign('name@fimk.fi', this.account1.secretPhrase);
-    
+
     // now the assigning account can assign only that id and only when providing the signature
     this.account1.setAccountIdentifier('name@fimk.fi', this.account1.id_rs, signature, this.account2.secretPhrase);
-    
+
     // and the name will assigned
-    jsAssert.assertEquals(['name@fimk.fi'], this.account1.getAccountIdentifiers());
+    jsAssert.assertEquals(['name@fimk.fi',"XHV3-9KPX-GR9Q-GJPYS"], this.account1.getAccountIdentifiers());
+  },
+  "Test if signature is same as lompsa client": function () {
+    var identifier      = 'aa@fimk.fi';
+    var secretPhrase    = 'ashamed those became either ourselves jealous stone dawn delicate cast ourselves surprise'
+    var signature       = Nxt.util.sign(identifier, secretPhrase);
+    var signatory       = 'FIM-BTEQ-8FK2-2HC4-7LZRC';
+    var publicKey       = 'dee5bc14b12ff8547ac1bd619f286d24cab4a330671a8e59bebcc96f96e8d069';
+
+    var signature_check = 'fc0363add05867022841911deb1906a0088b15e2540575532e2ce2b3712d080c5946f4b5bef036151c97e1caa662bbedfe9fa82d268280e5ea1a3f2fa8b80fb4';
+
+    jsAssert.assertEquals(signature, signature_check);
+
+    signatory = Nxt.util.convertRSAddress(signatory);
+
+    var arg = {
+      secretPhrase: this.account2.secretPhrase,
+      feeNQT: Nxt.util.convertToNQT('0.1'),
+      deadline: '1440',
+      recipient: signatory,
+      identifier: identifier,
+      signatory: signatory,
+      signature: signature,
+      recipientPublicKey: publicKey
+    };
+    var ret = Nxt.call('setAccountIdentifier', arg);
+    if (isError(ret)) { if (Nxt.verbose) { printError(ret); } return null; }
+    if (Nxt.enableAutoForge) { Nxt.generateBlock(); }
+
+    this.account1.setAccountIdentifier(identifier, signatory, signature, this.account2.secretPhrase);
+
   },
   "Other account can not set ID when providing signature for different ID": function () {
     var signature = Nxt.util.sign('name1@fimk.fi', this.account1.secretPhrase);
@@ -37,3 +67,5 @@ tests({
     
   }
 });
+
+
