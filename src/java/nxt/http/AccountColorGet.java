46225df00d7ad2428fae96d4f8996046898e0bb2
FIMKrypto <fimkrypto@gmail.com>
2015-12-29 15:28:07 +0100

    Account Color Get HTTP API

---
+++ src/java/nxt/http/AccountColorGet.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright © 2014-2016 Krypto Fin ry and FIMK Developers.                   *
+ *                                                                            *
+ * See the AUTHORS.txt, DEVELOPER-AGREEMENT.txt and LICENSE.txt files at      *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Nxt software, including this file, may be copied, modified, propagated,    *
+ * or distributed except according to the terms contained in the LICENSE.txt  *
+ * file.                                                                      *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+package nxt.http;
+
+import nxt.AccountColor;
+import nxt.NxtException;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class AccountColorGet extends CreateTransaction {
+
+    static final AccountColorGet instance = new AccountColorGet();
+
+    private AccountColorGet() {
+        super(new APITag[] {APITag.MOFO}, "accountColorId", "includeAccountInfo", "includeDescription");
+    }
+
+    @Override
+    JSONStreamAware processRequest(HttpServletRequest req) throws NxtException {
+        if (!AccountColor.getAccountColorEnabled()) {
+            return nxt.http.JSONResponses.FEATURE_NOT_AVAILABLE;
+        }
+
+        boolean includeAccountInfo = req.getParameter("includeAccountInfo") == "true";
+        boolean includeDescription = req.getParameter("includeDescription") == "true";
+        long accountColorId = ParameterParser.getUnsignedLong(req, "accountColorId", true);
+        AccountColor accountColor = AccountColor.getAccountColor(accountColorId);
+        if (accountColor == null) {
+          return nxt.http.JSONResponses.unknown("accountColor"); 
+        }
+        
+        return JSONData.accountColor(accountColor, includeAccountInfo, includeDescription);
+    }
+}
