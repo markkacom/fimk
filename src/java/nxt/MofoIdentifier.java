d7594e981191a889d8c834d8d788142876956790
FIMKrypto <fimkrypto@gmail.com>
2016-01-19 09:56:33 +0100

    Aff getSimilarId to identifier parser

--- src/java/nxt/MofoIdentifier.java
+++ src/java/nxt/MofoIdentifier.java
@@ -1,11 +1,13 @@
 package nxt;
 
 public class MofoIdentifier {
-  
+
     static final String[] DEFAULT_SERVERS = new String[] { "fimk.fi", "lompsa.com" };
-  
+
     private String normalizedId;
     private boolean isDefaultServer;
+    private String name;
+    private String server;
 
     @SuppressWarnings("serial")
     public static class InvalidAccountIdentifier extends RuntimeException {
@@ -13,22 +15,22 @@ public class MofoIdentifier {
             super("Invalid account identifier");
         }
     }
-    
+
     public MofoIdentifier(String id) {
         normalizedId = id.toLowerCase();
-        
+
         if (normalizedId.length() > Constants.MAX_ACCOUNT_IDENTIFIER_LENGTH) {
             throw new InvalidAccountIdentifier();
         }
-        
+
         String[] parts = normalizedId.split("@");
         if (parts.length != 2) {
             throw new InvalidAccountIdentifier();
         }
-        
-        String name = parts[0];
-        String server = parts[1];
-        
+
+        name = parts[0];
+        server = parts[1];
+
         for (int i=0; i<name.length(); i++) {
             if (Constants.ALLOWED_ACCOUNT_ID_NAME.indexOf(name.charAt(i)) < 0) {
                 throw new InvalidAccountIdentifier();
@@ -39,7 +41,7 @@ public class MofoIdentifier {
                 throw new InvalidAccountIdentifier();
             }
         }
-        
+
         for (int i=0; i<DEFAULT_SERVERS.length; i++) {
             if (DEFAULT_SERVERS[i].equals(server)) {
                 isDefaultServer = true;
@@ -48,12 +50,20 @@ public class MofoIdentifier {
         }
         isDefaultServer = false;
     }
-    
+
     public boolean getIsDefaultServer() {
         return isDefaultServer;
     }
-    
+
     public String getNormalizedId() {
         return normalizedId;
+    }
+
+    public String getSimilarId() {
+        if (isDefaultServer) {
+            int index = DEFAULT_SERVERS[0].equals(server) ? 1 : 0;
+            return name + '@' + DEFAULT_SERVERS[index];
+        }
+        return null;
     }
 }
