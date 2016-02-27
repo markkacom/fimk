492e0bedfbf1ecb3c3ca8a6799a7b9ffa0acb61e
FIMKrypto <fimkrypto@gmail.com>
2016-02-27 14:26:46 +0100

    Disable account listener and remove debugger statements

--- src/java/nxt/replicate/Replicate.java
+++ src/java/nxt/replicate/Replicate.java
@@ -51,25 +51,25 @@ public class Replicate {
     }
 
     private static void setupListeners() {
-        Account.addListener(account -> {
-            updateAccountBalance(account.getId(), account.getBalanceNQT());
-        }, Account.Event.BALANCE);
-
-        Account.addListener(account -> {
-            updateAccountUnconfirmedBalance(account.getId(), account.getUnconfirmedBalanceNQT());
-        }, Account.Event.UNCONFIRMED_BALANCE);
+//        Account.addListener(account -> {
+//            updateAccountBalance(account.getId(), account.getBalanceNQT());
+//        }, Account.Event.BALANCE);
+//
+//        Account.addListener(account -> {
+//            updateAccountUnconfirmedBalance(account.getId(), account.getUnconfirmedBalanceNQT());
+//        }, Account.Event.UNCONFIRMED_BALANCE);
 
         Account.addListener(account -> {
             updateAccountPublicKey(account.getId(), account.getPublicKey());
         }, Account.Event.PUBLIC_KEY);
 
-        Account.addAssetListener(accountAsset -> {
-            updateAccountAssetBalance(accountAsset.getAccountId(), accountAsset.getAssetId(), accountAsset.getQuantityQNT());
-        }, Account.Event.ASSET_BALANCE);
-
-        Account.addAssetListener(accountAsset -> {
-            updateAccountAssetUnconfirmedBalance(accountAsset.getAccountId(), accountAsset.getAssetId(), accountAsset.getUnconfirmedQuantityQNT());
-        }, Account.Event.UNCONFIRMED_ASSET_BALANCE);
+//        Account.addAssetListener(accountAsset -> {
+//            updateAccountAssetBalance(accountAsset.getAccountId(), accountAsset.getAssetId(), accountAsset.getQuantityQNT());
+//        }, Account.Event.ASSET_BALANCE);
+//
+//        Account.addAssetListener(accountAsset -> {
+//            updateAccountAssetUnconfirmedBalance(accountAsset.getAccountId(), accountAsset.getAssetId(), accountAsset.getUnconfirmedQuantityQNT());
+//        }, Account.Event.UNCONFIRMED_ASSET_BALANCE);
 
         Nxt.getBlockchainProcessor().addListener(block -> {
             blockPopped(block);
@@ -97,73 +97,62 @@ public class Replicate {
     }
 
     private static void updateAccountBalance(long accountId, long balanceNQT) {
-        Logger.logInfoMessage("Replicator updateAccountBalance");
         if (!db.getReplicator().updateAccountBalance(accountId, balanceNQT, Nxt.getBlockchain().getHeight())) {
-            Logger.logInfoMessage("Replicator updateAccountBalance failed");
+            throw new RuntimeException("Replicator updateAccountBalance failed");
         }
     }
 
     private static void updateAccountUnconfirmedBalance(long accountId, long unconfirmedBalanceNQT) {
-        Logger.logInfoMessage("Replicator updateAccountUnconfirmedBalance");
         if (!db.getReplicator().updateAccountUnconfirmedBalance(accountId, unconfirmedBalanceNQT, Nxt.getBlockchain().getHeight())) {
-            Logger.logInfoMessage("Replicator updateAccountUnconfirmedBalance failed");
+            throw new RuntimeException("Replicator updateAccountUnconfirmedBalance failed");
         }
     }
 
     private static void updateAccountPublicKey(long accountId, byte[] publicKey) {
-        Logger.logInfoMessage("Replicator updateAccountPublicKey");
         if (!db.getReplicator().updateAccountPublicKey(accountId, publicKey, Nxt.getBlockchain().getHeight())) {
-            Logger.logInfoMessage("Replicator updateAccountPublicKey failed");
+            throw new RuntimeException("Replicator updateAccountPublicKey failed");
         }
     }
 
     private static void updateAccountAssetBalance(long accountId, long assetId, long quantityQNT) {
-        Logger.logInfoMessage("Replicator updateAccountAssetBalance");
     }
 
     private static void updateAccountAssetUnconfirmedBalance(long accountId, long assetId, long unconfirmedQuantityQNT) {
-        Logger.logInfoMessage("Replicator updateAccountAssetUnconfirmedBalance");
     }
 
     private static void blockPopped(Block block) {
-        Logger.logInfoMessage("Replicator blockPopped");
         if (!db.getReplicator().blockPopped(block)) {
-            Logger.logInfoMessage("Replicator blockPopped failed");
+            throw new RuntimeException("Replicator blockPopped failed");
         }
     }
 
     private static void beforeBlockApply(Block block) {
-        Logger.logInfoMessage("Replicator beforeBlockApply " + block.getHeight());
         if (!db.getReplicator().beforeBlockApply(block)) {
-            Logger.logInfoMessage("Replicator beforeBlockApply failed");
+            throw new RuntimeException("Replicator beforeBlockApply failed");
         }
     }
 
     private static void rescanBegin(Block block) {
-        Logger.logInfoMessage("Replicator rescanBegin " + block.getHeight());
         if (!db.getReplicator().rescanBegin(block.getHeight())) {
-            Logger.logInfoMessage("Replicator rescanBegin failed");
+            throw new RuntimeException("Replicator rescanBegin failed");
         }
     }
 
     private static void addUnconfirmedTransaction(List<? extends Transaction> transactions) {
-        Logger.logInfoMessage("Replicator addUnconfirmedTransaction");
         if (!db.getReplicator().addedUnconfirmedTransactions(transactions)) {
-            Logger.logInfoMessage("Replicator addUnconfirmedTransaction failed");
+            throw new RuntimeException("Replicator addUnconfirmedTransaction failed");
         }
     }
 
     private static void removeUnconfirmedTransaction(List<? extends Transaction> transactions) {
-        Logger.logInfoMessage("Replicator removeUnconfirmedTransaction");
         if (!db.getReplicator().removedUnconfirmedTransactions(transactions)) {
-            Logger.logInfoMessage("Replicator removeUnconfirmedTransaction failed");
+            throw new RuntimeException("Replicator removeUnconfirmedTransaction failed");
         }
     }
 
     private static void addConfirmedTransaction(List<? extends Transaction> transactions) {
-        Logger.logInfoMessage("Replicator addConfirmedTransaction");
         if (!db.getReplicator().addedConfirmedTransactions(transactions)) {
-            Logger.logInfoMessage("Replicator addConfirmedTransaction failed");
+            throw new RuntimeException("Replicator addConfirmedTransaction failed");
         }
     }
 
