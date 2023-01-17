2abd8fde5ee2096b52a37f1c8fcf5aa07505503e
alex01011 <am777@yandex.ru>
2023-01-17 11:09:27 +0300

    Update fork height

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -173,8 +173,8 @@ public final class Constants {
      * Number of hit values generated to select the suitable
      */
     public static final int HITS_NUMBER = 17;
-    public static final int CONTROL_FORGING_TIME_BLOCK = isTestnet ? 1387 : 5_540_000;
-    public static final int POS_POP_REWARD_BLOCK = CONTROL_FORGING_TIME_BLOCK;
+    public static final int CONTROL_FORGING_TIME_BLOCK = isTestnet ? 1387 : 5_530_000;
+    public static final int POS_POP_REWARD_BLOCK = isTestnet ? 1387 : CONTROL_FORGING_TIME_BLOCK + 10_000;
     public static final int PRIVATE_ASSETS_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : 934504;
     public static final int PRIVATE_ASSETS_TIMESTAMP = isTestnet ? 1 : 68002834;
     public static final int ACCOUNT_IDENTIFIER_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : 934504;
