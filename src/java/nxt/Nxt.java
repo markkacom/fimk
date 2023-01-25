d81f053cba584ddf5174cb7416b7960b86c29e33
alex01011 <am777@yandex.ru>
2023-01-25 12:09:48 +0300

    Bump version

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -57,7 +57,7 @@ public final class Nxt {
 
     public static final String NXT_VERSION = "1.5.10";
     public static final String APPLICATION = "FIMK";
-    public static final String VERSION = "0.7.2-b7"; /* FIM Version. Note the Peers.MAX_VERSION_LENGTH*/
+    public static final String VERSION = "0.7.2-b8"; /* FIM Version. Note the Peers.MAX_VERSION_LENGTH*/
     public static final String MIN_VERSION = "0.6.1"; /* Blacklist everything up or below this version */
 
     private static volatile Time time = new Time.EpochTime();
