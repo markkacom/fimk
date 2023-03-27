b5256a380268a6f0623a030ed8fafceb50e1f50f
alex01011 <am777@yandex.ru>
2023-03-27 16:34:28 +0300

    Change POP POS Reward fork height

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -174,7 +174,7 @@ public final class Constants {
      */
     public static final int HITS_NUMBER = 17;
     public static final int CONTROL_FORGING_TIME_BLOCK = isTestnet ? 1387 : 5_530_000;
-    public static final int POS_POP_REWARD_BLOCK = isTestnet ? 1387 : 5_700_000;
+    public static final int POS_POP_REWARD_BLOCK = isTestnet ? 1387 : 5_800_000;
     public static final int PRIVATE_ASSETS_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : 934504;
     public static final int PRIVATE_ASSETS_TIMESTAMP = isTestnet ? 1 : 68002834;
     public static final int ACCOUNT_IDENTIFIER_BLOCK = isTestnet ? THIRD_BIRTH_BLOCK_TEST : 934504;
